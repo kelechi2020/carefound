932069b650ff057d085b5a69bc7266eb9c82cfe8
kelechi2020 <egbosikelechi@gmail.com>
2017-09-18 13:02:20 -0700

    url route coret

--- atikucare/urls.py
+++ atikucare/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'atikucare.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'atiku.views.page', name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^visualize/', 'atiku.view.visualize.page', name='visualize'),
-    url(r'^', 'atiku.views.page', name='home'),
+    url(r'^index/', 'atiku.views.page', name='home'),
     url(r'^getdetails/', 'atiku.view.getdetails.getdetails', name='states'),
 ]
